a40e466615ffad51cd87ece5ec2718cc77c961a8
PANCHO7532 <pancho7532@p7com.net>
2023-06-05 00:12:59 -0300

    Add cleanup script, just in case

---
+++ hardclean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Execute at your own risk.
+# This cleans every CMake generated file and object from project folders
+# ...so you can do an clean CMake rebuild afterwards.
+rm game/bin/client*
+rm game/bin/missionchooser*
+rm game/bin/server*
+rm -rf CMakeCache.txt && rm -rf CMakeFiles && rm cmake_install.cmake && rm Makefile && rm *.cbp && rm *.layout
+rm -rf src/game/client/CMakeCache.txt && rm -rf src/game/client/CMakeFiles && rm src/game/client/cmake_install.cmake && rm src/game/client/Makefile && rm src/game/client/*.cbp && rm src/game/client/*.layout
+rm -rf src/game/missionchooser/CMakeCache.txt && rm -rf src/game/missionchooser/CMakeFiles && rm src/game/missionchooser/cmake_install.cmake && rm src/game/missionchooser/Makefile && rm src/game/missionchooser/*.cbp && rm src/game/missionchooser/*.layout
+rm -rf src/game/server/CMakeCache.txt && rm -rf src/game/server/CMakeFiles && rm src/game/server/cmake_install.cmake && rm src/game/server/Makefile && rm src/game/server/*.cbp && rm src/game/server/*.layout
+echo "[INFO] Done!"
+exit
